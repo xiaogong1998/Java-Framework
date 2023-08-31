ceee70afba56abbb31d9f6feae6d6f33735f157f
xiaogong <15847682692@163.com>
2023-08-31 11:18:58 +0800

    review code

--- gong-wechat/gong-wechat-subscription/src/main/java/com/gong/wechat/subscription/properties/SubscriptionProperties.java
+++ gong-wechat/gong-wechat-subscription/src/main/java/com/gong/wechat/subscription/properties/SubscriptionProperties.java
@@ -1,13 +1,9 @@
 package com.gong.wechat.subscription.properties;
 
-import com.gong.wechat.subscription.entity.BaseTemplateIdEntity;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
